c34fc16c93dd196a3950906ad419d12cc70e267a
unknown <Rwilliams@Ray-PC.ames.ruralsoluxions.com>
2013-11-04 13:35:27 -0600

    Debugging POST

--- src/main/java/com/xpanxion/skeleton/controllers/HomeController.java
+++ src/main/java/com/xpanxion/skeleton/controllers/HomeController.java
@@ -6,10 +6,13 @@ package com.xpanxion.skeleton.controllers;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.xpanxion.skeleton.service.TestService;
+import com.xpanxion.skeleton.dto.entity.UserEntity;
+import com.xpanxion.skeleton.service.UserService;
 
 /**
  * @author brian
@@ -18,18 +21,23 @@ import com.xpanxion.skeleton.service.TestService;
 @Controller
 public class HomeController {
 
-    private TestService testService;
+    private UserService userService;
 
-    @RequestMapping("**/home")
-    public ModelAndView getHomePage() {
-        ModelAndView mAndV = new ModelAndView("home");
-        mAndV.addObject("test", this.testService.getTestBeans());
-        return mAndV;
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String makeLogin(@ModelAttribute UserEntity user, Model m) {
+        m.addAttribute("user", new UserEntity());
+        return "home";
     }
 
     @Resource
-    public void setTestService(TestService service) {
-        this.testService = service;
+    public void setUserService(UserService service) {
+        this.userService = service;
+    }
+
+    @RequestMapping(value = "form", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute UserEntity user, Model m) {
+        m.addAttribute("message", "successfully loggged in");
+        return "home";
     }
 
 }
--- src/main/webapp/jsp/home.jsp
+++ src/main/webapp/jsp/home.jsp
@@ -1,4 +1,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+
+<!DOCTYPE HTML>
 <html>
 <head>
 	<title>Home Page</title>
@@ -6,9 +9,16 @@
 <body>
 <h1>Under Construction!!!</h1>
 <table>
-<c:forEach var="bean" items="${test}">
-<tr><td>${bean.id }</td><td>${bean.text }</td></tr>
-</c:forEach>
+<tr><td><form:form modelAttribute="user">
+	<label for="usernameInput">username: </label>
+	<form:input path="username" id="usernameInput" /></td>
+	<td>
+	<label for="passwordInput">password:</label>
+	<form:input path="password" name = "passwordInput" /></td>
+	</form:form>
+	<td><input type="submit" value="Submit" /></td>
+	<td><c:if test="${not empty message}"><div class="message green">${message}}</div></c:if></td> </tr>
+	
 </table>
 </body>
 </html>
